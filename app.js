dd342e1c9636a4484946c72dd1b3936aa45b40f1
agustrinaldokurniawan <agustkurniawan010899@gmail.com>
2019-12-26 13:43:21 +0700

    first

---
+++ app.js
@@ -0,0 +1,100 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mysql = require("mysql");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "nodelogin"
+});
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/signin", (req, res, next) => {
+  res.render("login");
+});
+
+app.post("/auth", (request, response) => {
+  var username = request.body.username;
+  var password = request.body.password;
+  if (username && password) {
+    connection.query(
+      "SELECT * FROM accounts WHERE username = ? AND password = ?",
+      [username, password],
+      function(error, results, fields) {
+        if (results.length > 0) {
+          request.session.loggedin = true;
+          request.session.username = username;
+          response.redirect("/home");
+        } else {
+          response.send("Incorrect Username and/or Password!");
+          console.log(username + password);
+        }
+        response.end();
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+    response.end();
+  }
+});
+
+app.get("/home", function(request, response) {
+  if (request.session.loggedin) {
+    response.send("Welcome back, " + request.session.username + "!");
+  } else {
+    response.send("Please login to view this page!");
+  }
+  response.end();
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
